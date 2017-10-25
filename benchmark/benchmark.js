1623709e997b56aa7be6bb81a02e1d7543e6239f
Trygve Lie <post@trygve-lie.com>
2017-10-25 16:24:28 +0200

    #47 - Basic benchmark script

---
+++ benchmark/benchmark.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const benchmark = require('benchmark');
+const Client = require('../');
+
+const suite = new benchmark.Suite();
+
+const add = (name, fn) => {
+    suite.add(name, fn);
+};
+
+const client = new Client();
+const component = client.register({
+    name: 'foo',
+    uri: 'http://localhost:8100/manifest.json',
+});
+
+const times = 4;
+for (let i = 0; i < times; i++) {
+    add(`fetch() - Run ${i}`, {
+        defer: true,
+        fn(deferred) {
+            component
+                .fetch({})
+                .then(() => {
+                    deferred.resolve();
+                })
+                .catch(error => {
+                    console.log(error);
+                    deferred.resolve();
+                });
+        },
+    });
+}
+
+suite
+    .on('cycle', ev => {
+        console.log(ev.target.toString());
+        if (ev.target.error) {
+            console.error(ev.target.error);
+        }
+    })
+    .run();
---
+++ benchmark/memory-profiling.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const memwatch = require('memwatch-next');
+const Client = require('../');
+const http = require('http');
+const url = require('url');
+
+memwatch.on('leak', info => {
+    console.error('Memory leak detected:', info);
+});
+
+memwatch.on('stats', info => {
+    console.info('Memory stats:', info);
+});
+
+process.on('SIGINT', () => process.exit(1));
+process.on('SIGTERM', () => process.exit(1));
+
+const client = new Client();
+const component = client.register({
+    name: 'foo',
+    uri: 'http://localhost:8100/manifest.json',
+});
+
+http
+    .createServer((req, res) => {
+        const u = url.parse(req.url, true);
+
+        if (u.pathname === '/') {
+            component
+                .fetch({})
+                .then(content => {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html; charset=utf-8',
+                    });
+                    res.end(content);
+                })
+                .catch(() => {
+                    res.writeHead(500, {
+                        'Content-Type': 'text/plain',
+                    });
+                    res.end('Internal server error');
+                });
+            return;
+        }
+
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
+        return;
+    })
+    .listen(8000);
+
+console.log('Server listening on port 8000');

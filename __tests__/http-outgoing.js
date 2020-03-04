4dc337423a7780c8b0455f7a79ce5f42b2e10ab3
Richard Walker <digitalsadhu@gmail.com>
2020-03-05 12:54:38 +1300

    port http-outgoing.js

--- __tests__/http-outgoing.js
+++ __tests__/http-outgoing.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { test } = require('tap');
 const { destinationBufferStream } = require('@podium/test-utils');
 const isStream = require('is-stream');
 const HttpOutgoing = require('../lib/http-outgoing');
@@ -16,56 +17,64 @@ const RESOURCE_OPTIONS = {
  * Constructor
  */
 
-test('HttpOutgoing() - object tag - should be PodletClientHttpOutgoing', () => {
+test('HttpOutgoing() - object tag - should be PodletClientHttpOutgoing', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS);
-    expect(Object.prototype.toString.call(outgoing)).toEqual(
-        '[object PodletClientHttpOutgoing]'
+    t.equal(
+        Object.prototype.toString.call(outgoing),
+        '[object PodletClientHttpOutgoing]',
     );
+    t.end();
 });
 
-test('HttpOutgoing() - "options.uri" not provided to constructor - should throw', () => {
-    expect(() => {
+test('HttpOutgoing() - "options.uri" not provided to constructor - should throw', t => {
+    t.throws(() => {
         // eslint-disable-next-line no-unused-vars
         const outgoing = new HttpOutgoing();
-    }).toThrowError(
-        'you must pass a URI in "options.uri" to the HttpOutgoing constructor'
-    );
+    }, 'you must pass a URI in "options.uri" to the HttpOutgoing constructor');
+    t.end();
 });
 
-test('HttpOutgoing() - should be a PassThrough stream', () => {
+test('HttpOutgoing() - should be a PassThrough stream', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
     // NOTE: PassThrough is just a transform stream pushing all chunks through. is-stream has no PassThrough check.
-    expect(isStream.transform(outgoing)).toBe(true);
+    t.ok(isStream.transform(outgoing));
+    t.end();
 });
 
-test('HttpOutgoing() - set "uri" - should be accessable on "this.manifestUri"', () => {
+test('HttpOutgoing() - set "uri" - should be accessable on "this.manifestUri"', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS);
-    expect(outgoing.manifestUri).toBe(RESOURCE_OPTIONS.uri);
+    t.equal(outgoing.manifestUri, RESOURCE_OPTIONS.uri);
+    t.end();
 });
 
-test('HttpOutgoing() - set "reqOptions" - should be persisted on "this.reqOptions"', () => {
+test('HttpOutgoing() - set "reqOptions" - should be persisted on "this.reqOptions"', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
-    expect(outgoing.reqOptions.pathname).toBe('a');
-    expect(outgoing.reqOptions.query.b).toBe('c');
+    t.equal(outgoing.reqOptions.pathname, 'a');
+    t.equal(outgoing.reqOptions.query.b, 'c');
+    t.end();
 });
 
-test('HttpOutgoing() - "this.manifest" should be {_fallback: ""}', () => {
+test('HttpOutgoing() - "this.manifest" should be {_fallback: ""}', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
-    expect(outgoing.manifest).toEqual({ _fallback: '' });
+    t.same(outgoing.manifest, { _fallback: '' });
+    t.end();
 });
 
-test('HttpOutgoing() - get manifestUri - should return URI to manifest', () => {
+test('HttpOutgoing() - get manifestUri - should return URI to manifest', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS);
-    expect(outgoing.manifestUri).toBe(RESOURCE_OPTIONS.uri);
+    t.equal(outgoing.manifestUri, RESOURCE_OPTIONS.uri);
+    t.end();
 });
 
-test('HttpOutgoing() - call .pushFallback() - should push the fallback content on the stream', () => {
+test('HttpOutgoing() - call .pushFallback() - should push the fallback content on the stream', t => {
+    t.plan(1);
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
     outgoing.manifest = {};
     outgoing.fallback = '<p>haz fallback</p>';
 
     const to = destinationBufferStream(result => {
-        expect(result).toBe('<p>haz fallback</p>');
+        t.equal(result, '<p>haz fallback</p>');
+        t.end();
     });
 
     outgoing.pipe(to);
@@ -73,44 +82,50 @@ test('HttpOutgoing() - call .pushFallback() - should push the fallback content o
     outgoing.pushFallback();
 });
 
-test('HttpOutgoing() - "options.throwable" is not defined - "this.throwable" should be "false"', () => {
+test('HttpOutgoing() - "options.throwable" is not defined - "this.throwable" should be "false"', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
-    expect(outgoing.throwable).toBeFalsy();
+    t.notOk(outgoing.throwable);
+    t.end();
 });
 
-test('HttpOutgoing() - "options.throwable" is defined to be true - "this.throwable" should be "true"', () => {
+test('HttpOutgoing() - "options.throwable" is defined to be true - "this.throwable" should be "true"', t => {
     const options = {
         uri: 'http://example.org',
         throwable: true,
     };
     const outgoing = new HttpOutgoing(options, REQ_OPTIONS);
-    expect(outgoing.throwable).toBeTruthy();
+    t.ok(outgoing.throwable);
+    t.end();
 });
 
-test('HttpOutgoing() - "options.resolveCss" is not defined - "this.resolveCss" should be "false"', () => {
+test('HttpOutgoing() - "options.resolveCss" is not defined - "this.resolveCss" should be "false"', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
-    expect(outgoing.resolveCss).toBeFalsy();
+    t.notOk(outgoing.resolveCss);
+    t.end();
 });
 
-test('HttpOutgoing() - "options.resolveCss" is defined to be true - "this.resolveCss" should be "true"', () => {
+test('HttpOutgoing() - "options.resolveCss" is defined to be true - "this.resolveCss" should be "true"', t => {
     const options = {
         uri: 'http://example.org',
         resolveCss: true,
     };
     const outgoing = new HttpOutgoing(options, REQ_OPTIONS);
-    expect(outgoing.resolveCss).toBeTruthy();
+    t.ok(outgoing.resolveCss);
+    t.end();
 });
 
-test('HttpOutgoing() - "options.resolveJs" is not defined - "this.resolveJs" should be "false"', () => {
+test('HttpOutgoing() - "options.resolveJs" is not defined - "this.resolveJs" should be "false"', t => {
     const outgoing = new HttpOutgoing(RESOURCE_OPTIONS, REQ_OPTIONS);
-    expect(outgoing.resolveJs).toBeFalsy();
+    t.notOk(outgoing.resolveJs);
+    t.end();
 });
 
-test('HttpOutgoing() - "options.resolveJs" is defined to be true - "this.resolveJs" should be "true"', () => {
+test('HttpOutgoing() - "options.resolveJs" is defined to be true - "this.resolveJs" should be "true"', t => {
     const options = {
         uri: 'http://example.org',
         resolveJs: true,
     };
     const outgoing = new HttpOutgoing(options, REQ_OPTIONS);
-    expect(outgoing.resolveJs).toBeTruthy();
+    t.ok(outgoing.resolveJs);
+    t.end();
 });

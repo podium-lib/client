0556af121813adbb62666a1f4556264c2d6241f6
Richard Walker <digitalsadhu@gmail.com>
2020-03-05 14:08:17 +1300

    port resolver.js tests

--- __tests__/resolver.js
+++ __tests__/resolver.js
@@ -1,20 +1,22 @@
 'use strict';
 
+const { test } = require('tap');
 const TtlMemCache = require('ttl-mem-cache');
 const Resolver = require('../lib/resolver');
 
-test('resolver() - object tag - should be PodletClientResolver', () => {
+test('resolver() - object tag - should be PodletClientResolver', t => {
     const resolver = new Resolver(new TtlMemCache());
-    expect(Object.prototype.toString.call(resolver)).toEqual(
-        '[object PodletClientResolver]'
+    t.equal(
+        Object.prototype.toString.call(resolver),
+        '[object PodletClientResolver]',
     );
+    t.end();
 });
 
-test('resolver() - "registry" not provided to constructor - should throw', () => {
-    expect(() => {
+test('resolver() - "registry" not provided to constructor - should throw', t => {
+    t.throws(() => {
         // eslint-disable-next-line no-unused-vars
         const resolver = new Resolver();
-    }).toThrowError(
-        'you must pass a "registry" object to the PodletClientResolver constructor'
-    );
+    }, 'you must pass a "registry" object to the PodletClientResolver constructor');
+    t.end();
 });

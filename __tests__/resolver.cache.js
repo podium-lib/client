9e7bf18fa332b3831d0a3e62d6fbd543b690608e
Richard Walker <digitalsadhu@gmail.com>
2020-03-05 13:42:42 +1300

    port resolver.cache.js test

--- __tests__/resolver.cache.js
+++ __tests__/resolver.cache.js
@@ -2,21 +2,23 @@
 
 'use strict';
 
+const { test } = require('tap');
 const Cache = require('../lib/resolver.cache');
 const TtlMemCache = require('ttl-mem-cache');
 
-test('resolver.cache() - object tag - should be PodletClientCacheResolver', () => {
+test('resolver.cache() - object tag - should be PodletClientCacheResolver', t => {
     const cache = new Cache(new TtlMemCache());
-    expect(Object.prototype.toString.call(cache)).toEqual(
+    t.equal(
+        Object.prototype.toString.call(cache),
         '[object PodletClientCacheResolver]',
     );
+    t.end();
 });
 
-test('resolver.cache() - "registry" not provided to constructor - should throw', () => {
-    expect(() => {
+test('resolver.cache() - "registry" not provided to constructor - should throw', t => {
+    t.throws(() => {
         // eslint-disable-next-line no-unused-vars
         const cache = new Cache();
-    }).toThrowError(
-        'you must pass a "registry" object to the PodletClientCacheResolver constructor',
-    );
+    }, 'you must pass a "registry" object to the PodletClientCacheResolver constructor');
+    t.end();
 });

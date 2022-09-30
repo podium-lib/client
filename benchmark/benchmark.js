c2e84f6f1978134ae99adb4afa9cd7ccb35c0b93
Trygve Lie <trygve.lie@finn.no>
2022-09-30 23:50:09 +0200

    test: converted benchmarks into esm

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -2,10 +2,10 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable import/no-extraneous-dependencies */
 
-'use strict';
-
-const Benchmark = require('benchmark');
-const Client = require("..");
+import Benchmark from 'benchmark';
+import { HttpIncoming } from '@podium/utils';
+import PodiumClient from '../lib/client.js';
+import { request } from 'undici';
 
 // Helper for wrapping async functions
 // Ref: https://github.com/bestiejs/benchmark.js/issues/176#issuecomment-812163728
@@ -19,17 +19,33 @@ function p(fn) {
     }
   }
 
-const client = new Client({name: 'bar'});
-const component = client.register({
+const podiumClient = new PodiumClient({name: 'bar'});
+const component = podiumClient.register({
     name: 'foo',
     uri: 'http://localhost:8100/manifest.json',
+});
+
+const incoming = new HttpIncoming({
+    headers: {
+        host: 'localhost:3030',
+    },
+    hostname: 'localhost',
+    url: '/some/path?foo=bar',
 });
 
 const suite = new Benchmark.Suite()
 
 suite
+  .add('base', p(async () => {
+    const {
+        statusCode,
+        headers,
+        body
+    } = await request('http://localhost:8100/index.html')
+    const data = await body.text();
+  }))
   .add('.fetch()', p(async () => {
-    await component.fetch({});
+    await component.fetch(incoming);
   }))
   .on('cycle', ev => console.log(String(ev.target)))
   .run({ delay: 0 });

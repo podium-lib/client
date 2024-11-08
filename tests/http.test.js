69b768877fb54c81961a033bad769b5143b3c086
William Killerud <william.killerud@schibsted.com>
2024-11-08 13:55:35 +0100

    test: run test with undici function

--- tests/http.test.js
+++ tests/http.test.js
@@ -1,36 +1,24 @@
 import { test } from 'node:test';
 import { rejects } from 'node:assert';
+import express from 'express';
 import HTTP from '../lib/http.js';
 
 test('should abort the request if it takes longer than the timeout', async () => {
-    // Mock the undici.Client's request method
-    const mockRequestFn = async (url, { signal }) => {
-        return new Promise((resolve, reject) => {
-            // Simulate a delay longer than the timeout
-            setTimeout(() => {
-                if (signal.aborted) {
-                    const abortError = new Error(
-                        'Request aborted due to timeout',
-                    );
-                    abortError.name = 'AbortError';
-                    reject(abortError);
-                } else {
-                    resolve({
-                        statusCode: 200,
-                        headers: {},
-                        body: 'OK',
-                    });
-                }
-            }, 2000); // 2 seconds delay
-        });
-    };
+    const app = express();
+    app.get('/test', (req, res) => {
+        setTimeout(() => {
+            res.send('OK');
+        }, 2000); // longer than the default request timeout
+    });
+    const server = app.listen({
+        port: 4321,
+        host: 'localhost',
+    });
 
-    // @ts-ignore
-    const http = new HTTP(mockRequestFn);
-    const url = 'https://example.com/test';
+    const http = new HTTP();
+    const url = `http://localhost:4321/test`;
     const options = {
         method: /** @type {'GET'} */ ('GET'),
-        timeout: 1000, // 1 second timeout
     };
 
     // Assert that the request is rejected with an AbortError
@@ -41,4 +29,5 @@ test('should abort the request if it takes longer than the timeout', async () =>
             err.message === 'Request aborted due to timeout',
         'Expected request to be aborted due to timeout',
     );
+    server.close();
 });

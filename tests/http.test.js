73416331b932e233c52e5df4ef63535c172696b6
William Killerud <william.killerud@schibsted.com>
2024-11-08 15:18:58 +0100

    fix: use AbortSignal.timeout()
    Calling abortController.abort() in a setTimeout context raises
    an "uncatchable" error, not a promise rejection like we expect.

--- tests/http.test.js
+++ tests/http.test.js
@@ -1,33 +1,40 @@
-import { test } from 'node:test';
-import { rejects } from 'node:assert';
+import tap from 'tap';
 import express from 'express';
 import HTTP from '../lib/http.js';
 
-test('should abort the request if it takes longer than the timeout', async () => {
-    const app = express();
-    app.get('/test', (req, res) => {
-        setTimeout(() => {
-            res.send('OK');
-        }, 2000); // longer than the default request timeout
-    });
-    const server = app.listen({
-        port: 4321,
-        host: 'localhost',
-    });
+tap.test(
+    'should abort the request if it takes longer than the timeout',
+    (t) => {
+        const app = express();
+        app.get('/', (req, res) => {
+            setTimeout(() => {
+                res.send('OK');
+            }, 2000); // longer than the default request timeout
+        });
 
-    const http = new HTTP();
-    const url = `http://localhost:4321/test`;
-    const options = {
-        method: /** @type {'GET'} */ ('GET'),
-    };
+        const server = app.listen(
+            {
+                host: '0.0.0.0',
+                port: 0,
+            },
+            () => {
+                const url = `http://${server.address().address}:${server.address().port}/`;
+                const options = {
+                    method: /** @type {'GET'} */ ('GET'),
+                };
 
-    // Assert that the request is rejected with an AbortError
-    await rejects(
-        http.request(url, options),
-        (/** @type {Error} */ err) =>
-            err.name === 'AbortError' &&
-            err.message === 'Request aborted due to timeout',
-        'Expected request to be aborted due to timeout',
-    );
-    server.close();
-});
+                t.rejects(
+                    async () => {
+                        const http = new HTTP();
+                        await http.request(url, options);
+                    },
+                    'Request aborted due to timeout',
+                    'Expected request to be aborted due to timeout',
+                ).finally(() => {
+                    server.close();
+                    t.end();
+                });
+            },
+        );
+    },
+);

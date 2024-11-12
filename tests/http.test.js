5bfe1822e4e005d14cadfe8dad78aa1d40c7eb9c
William Killerud <william.killerud@schibsted.com>
2024-11-12 08:49:00 +0100

    test: don't mock client under test

--- tests/http.test.js
+++ tests/http.test.js
@@ -1,44 +1,42 @@
-import { test } from 'node:test';
-import { rejects } from 'node:assert';
+import tap from 'tap';
+import express from 'express';
 import HTTP from '../lib/http.js';
 
-test('should abort the request if it takes longer than the timeout', async () => {
-    // Mock the undici.Client's request method
-    const mockRequestFn = async (url, { signal }) => {
-        return new Promise((resolve, reject) => {
-            // Simulate a delay longer than the timeout
+tap.test(
+    'should abort the request if it takes longer than the timeout',
+    (t) => {
+        const app = express();
+        app.get('/', (req, res) => {
             setTimeout(() => {
-                if (signal.aborted) {
-                    const abortError = new Error(
-                        'Request aborted due to timeout',
-                    );
-                    abortError.name = 'AbortError';
-                    reject(abortError);
-                } else {
-                    resolve({
-                        statusCode: 200,
-                        headers: {},
-                        body: 'OK',
-                    });
-                }
-            }, 2000); // 2 seconds delay
+                res.send('OK');
+            }, 2000); // longer than the default request timeout
         });
-    };
 
-    // @ts-ignore
-    const http = new HTTP(mockRequestFn);
-    const url = 'https://example.com/test';
-    const options = {
-        method: /** @type {'GET'} */ ('GET'),
-        timeout: 1000, // 1 second timeout
-    };
+        const server = app.listen(
+            {
+                host: '0.0.0.0',
+                port: 0,
+            },
+            () => {
+                const url = `http://${server.address().address}:${server.address().port}/`;
+                const options = {
+                    method: /** @type {'GET'} */ ('GET'),
+                };
 
-    // Assert that the request is rejected with an AbortError
-    await rejects(
-        http.request(url, options),
-        (/** @type {Error} */ err) =>
-            err.name === 'AbortError' &&
-            err.message === 'Request aborted due to timeout',
-        'Expected request to be aborted due to timeout',
-    );
-});
+                t.rejects(
+                    async () => {
+                        const http = new HTTP();
+                        await http.request(url, options);
+                    },
+                    {
+                        name: 'AbortError',
+                        message: 'This operation was aborted',
+                    },
+                ).finally(() => {
+                    server.close();
+                    t.end();
+                });
+            },
+        );
+    },
+);
